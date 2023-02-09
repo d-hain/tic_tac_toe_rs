c98e7f128a5bd361fbd63b8064e92f7055fb350d
dhain <d.hain@gmx.at>
2023-02-09 21:27:51 +0100

    Added SDL2 and made it work

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,53 @@
+use std::time::Duration;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::Duration;
+use sdl2::EventPump;
+use sdl2::render::WindowCanvas;
 
-pub fn main() {
+fn main() {
+    let (mut canvas, mut event_pump) = setup_sdl();
+    
+    // Game Loop
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running;
+                }
+                _ => {}
+            }
+        }
+        
+        update(&mut canvas);
+    }
+}
+
+fn update(canvas: &mut WindowCanvas) {
+    canvas.clear();
+
+    
+
+    canvas.present();
+    std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+}
+
+fn setup_sdl() -> (WindowCanvas, EventPump) {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window("tic_tac_toe_rs", 600, 600)
         .position_centered()
         .build()
         .expect("Window creation went terribly wrong!");
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.set_draw_color(Color::RGB(69, 69, 69));
     canvas.clear();
     canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-    'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
-            }
-        }
-        // The rest of the game loop goes here...
+    let event_pump = sdl_context.event_pump().unwrap();
 
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
+    (canvas, event_pump)
 }
