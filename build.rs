1abcbe313d89162e34d0e0548a955e12b85fbf4f
dhain <d.hain@gmx.at>
2023-02-11 23:52:01 +0100

    Displaying Field (Text of Cells)
    - added SDL2_ttf
    
    - added `ComicSansMS3.ttf`
    
    - implemented From<Sign> for &str
    
    - `Field.draw()` method now displays text of signs (not done)
    
    - added font to `GameState`
    
    - created `get_text_texture` function to create a texture from a string

--- build.rs
+++ build.rs
@@ -10,8 +10,7 @@ fn main() {
         if target.contains("msvc") {
             lib_dir.push("msvc");
             dll_dir.push("msvc");
-        }
-        else {
+        } else {
             lib_dir.push("gnu-mingw");
             dll_dir.push("gnu-mingw");
         }
@@ -20,13 +19,12 @@ fn main() {
         if target.contains("x86_64") {
             lib_dir.push("64");
             dll_dir.push("64");
-        }
-        else {
+        } else {
             lib_dir.push("32");
             dll_dir.push("32");
         }
         println!("cargo:rustc-link-search=all={}", lib_dir.display());
-        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir")  {
+        for entry in std::fs::read_dir(dll_dir).expect("Can't read DLL dir") {
             let entry_path = entry.expect("Invalid fs entry").path();
             let file_name_result = entry_path.file_name();
             let mut new_file_path = manifest_dir.clone();
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use anyhow::Result;
+#![warn(clippy::unwrap_used)]
+
+use anyhow::{Result, Ok, anyhow, Context};
 use std::time::Duration;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::EventPump;
 use sdl2::rect::Rect;
-use sdl2::render::WindowCanvas;
+use sdl2::render::{Texture, TextureCreator, TextureValueError, WindowCanvas};
+use sdl2::ttf::Font;
+use sdl2::ttf::{FontStyle, Sdl2TtfContext};
+use sdl2::video::WindowContext;
 
 /// [`Sign`] to represent the players.
 #[derive(Copy, Clone)]
@@ -14,11 +19,11 @@ enum Sign {
     O,
 }
 
-impl ToString for Sign {
-    fn to_string(&self) -> String {
-        match self {
-            Sign::X => "X".to_string(),
-            Sign::O => "O".to_string(),
+impl From<Sign> for &str {
+    fn from(sign: Sign) -> Self {
+        match sign {
+            Sign::X => "X",
+            Sign::O => "O",
         }
     }
 }
@@ -42,14 +47,23 @@ impl Field {
     }
 
     /// Draw the [`Field`] to the given `canvas`.
-    fn draw(&self, canvas: &mut WindowCanvas) {
+    fn draw(&self, font: &Font, canvas: &mut WindowCanvas) -> Result<()> {
         let window_size = canvas.window().size();
         let cell_size = window_size.0 / 5;
         let padding = cell_size / 4;
 
         for (row_idx, row) in self.0.iter().enumerate() {
             for (col_idx, cell) in row.iter().enumerate() {
-                //TODO: display Sign
+                // Display Sign
+                let texture_creator = canvas.texture_creator();
+                let sign_text = match cell.0 {
+                    Some(sign) => sign.into(),
+                    None => " ",
+                };
+                let sign_texture = get_text_texture(sign_text, font, &texture_creator).context("Creating texture for player Sign")?;
+                let target = Rect::new(0, 0, 200, 200);
+                canvas.copy(&sign_texture, None, Some(target)).expect("Displaying texture for player Sign"); //TODO: Really do not want to use expect here
+
                 canvas.set_draw_color(Color::RGB(200, 0, 255));
                 canvas.fill_rect(Rect::new(
                     (cell_size * col_idx as u32 + padding * col_idx as u32) as i32,
@@ -60,20 +74,28 @@ impl Field {
                 canvas.set_draw_color(BACKGROUND_COLOR);
             }
         }
+
+        Ok(())
     }
 }
 
-struct GameState {
+struct GameState<'a> {
+    font: Font<'a, 'a>,
     field: Field,
 }
 
 const BACKGROUND_COLOR: Color = Color::RGB(69, 69, 69);
 
 fn main() {
-    let (mut canvas, mut event_pump) = setup_sdl();
+    let (mut canvas, mut event_pump, ttf_context) = setup_sdl();
+
+    // Setup GameState
+    let font = ttf_context.load_font("assets/ComicSansMS3.ttf", 28).expect("Loading font");
     let mut game_state = GameState {
+        font,
         field: Field::empty(3),
     };
+    game_state.field.0[0][0] = Cell(Some(Sign::X)); // TODO: temp
 
     // Game Loop
     'running: loop {
@@ -95,7 +117,7 @@ fn main() {
 fn update(canvas: &mut WindowCanvas, game_state: &mut GameState) -> Result<()> {
     canvas.clear();
 
-    game_state.field.draw(canvas);
+    game_state.field.draw(&game_state.font, canvas).context("Drawing game Field")?;
 
     canvas.present();
     std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
@@ -104,9 +126,10 @@ fn update(canvas: &mut WindowCanvas, game_state: &mut GameState) -> Result<()> {
 }
 
 /// Setup everything that has to do with SDL2
-fn setup_sdl() -> (WindowCanvas, EventPump) {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+fn setup_sdl() -> (WindowCanvas, EventPump, Sdl2TtfContext) {
+    let sdl_context = sdl2::init().expect("Initializing SDL2");
+    let video_subsystem = sdl_context.video().expect("Initializing Video Subsystem");
+    let ttf_context = sdl2::ttf::init().expect("Initializing TTF Context");
 
     let window = video_subsystem.window("tic_tac_toe_rs", 600, 600)
         .position_centered()
@@ -114,12 +137,26 @@ fn setup_sdl() -> (WindowCanvas, EventPump) {
         .build()
         .expect("Window creation went terribly wrong!");
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window.into_canvas().build().expect("Building Canvas");
 
     canvas.set_draw_color(BACKGROUND_COLOR);
     canvas.clear();
     canvas.present();
-    let event_pump = sdl_context.event_pump().unwrap();
+    let event_pump = sdl_context.event_pump().expect("Getting Event Dump");
 
-    (canvas, event_pump)
+    (canvas, event_pump, ttf_context)
+}
+
+fn get_text_texture<'a>(
+    text: impl Into<&'a str>,
+    font: &'a Font<'a, 'a>,
+    texture_creator: &'a TextureCreator<WindowContext>,
+) -> Result<Texture<'a>> {
+    let surface = font
+        .render(text.into())
+        .blended(Color::RGB(0, 255, 0))?;
+    let texture = texture_creator
+        .create_texture_from_surface(&surface)?;
+
+    Ok(texture)
 }
